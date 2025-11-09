b21c6e9feee40da9da9fbb716243ff99cb872848
jinxin001 <jinxin001@bytedance.com>
2025-11-09 09:41:08 +0800

    chore: js sdk

---
+++ sdk/js/rslib.config.ts
@@ -0,0 +1,40 @@
+import { defineConfig } from '@rslib/core';
+
+export default defineConfig({
+  source: {
+    entry: {
+      index: ['./src/**/*.ts', '!./src/**/*.{test,bench}.ts'],
+    },
+  },
+  lib: [
+    {
+      format: 'esm',
+      syntax: 'es2021',
+      bundle: false,
+      dts: {
+        bundle: false,
+        abortOnError: false,
+      },
+      output: {
+        distPath: './dist/esm',
+      },
+    },
+    {
+      format: 'cjs',
+      syntax: 'es2021',
+      bundle: false,
+      dts: {
+        bundle: false,
+        abortOnError: false,
+      },
+      output: {
+        distPath: './dist/cjs',
+      },
+    },
+  ],
+  output: {
+    target: 'node',
+    cleanDistPath: true,
+    sourceMap: true,
+  },
+});
